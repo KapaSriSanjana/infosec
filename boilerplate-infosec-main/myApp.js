27b627ee1b88d3b779253a0228da8238e16b7602
K Sri Sanjana <srisanjanakapa@gmail.com>
2025-01-25 11:51:55 +0530

    use yarn

--- boilerplate-infosec-main/myApp.js
+++ boilerplate-infosec-main/myApp.js
@@ -2,60 +2,60 @@ const express = require('express');
 const app = express();
 
 let helmet = require('helmet')
+const PORT = process.env.PORT || 3030;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+module.exports=app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
+app.use('/_api',api);
+app.get("/",function (request, response){
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(port, ()=>{
+  console.log(`your app is listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
